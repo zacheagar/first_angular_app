2057046f8c0f2b674c3f8ca2e57b6039236c87c8
zach.eagar <zach.eagar@planswift.com>
2018-01-26 12:00:01 -0700

    added task data and improved functionality

--- scripts/app.js
+++ scripts/app.js
@@ -3,5 +3,14 @@ angular.module('todoListApp', [])
      $scope.helloWorld = function(){
        console.log("Hello world function");  
      };
+    $scope.todos = [
+    
+      {"name":"clean the house"},
+      {"name":"code"},
+      {"name":"groceries"},
+      {"name":"pay bills"},
+      {"name":"have fun"},
+
+    ]
 
     });
