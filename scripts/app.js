d0fc4a3a5515174792e4a7178e7a83e93919a292
zach.eagar <zach.eagar@planswift.com>
2018-01-29 11:47:20 -0700

    added mock crud operations

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,39 @@
 angular.module('todoListApp', [])
- .controller('mainCtrl',function($scope){
-     $scope.helloWorld = function(){
-       console.log("Hello world function");  
+ .controller('mainCtrl',function($scope,$http,dataService){
+  
+  $scope.helloWorld = dataService.helloWorld;   
+  
+  $scope.addTodo = function(){
+    var todo = {name: "This is a new todo."};
+    $scope.todos.push(todo);
+  }
+
+     dataService.getTodos(function(response){
+       console.log(response.data);
+       $scope.todos = response.data;
+     });
+     $scope.deleteTodo = function(todo, $index){
+      dataService.deleteTodo(todo);
+      $scope.todos.splice($index, 1);
      };
-    $scope.todos = [
-    
-      {"name":"clean the house"},
-      {"name":"code"},
-      {"name":"groceries"},
-      {"name":"pay bills"},
-      {"name":"have fun"},
+    $scope.saveTodo = function(todo){
+      dataService.saveTodo(todo);
+    }
+    })
+    .service('dataService',function($http){
 
-    ]
+      this.helloWorld = function(){
+        console.log('This is service');
+      };
 
+      this.getTodos = function(callback){ 
+        $http.get('mock/todos.json')
+        .then(callback)
+      };
+      this.deleteTodo = function(todo){
+       console.log("The" + todo.name + " has been deleted!")
+      };
+      this.saveTodo = function(todo){
+       console.log("The" + todo.name + " has been saved!")
+      }
     });
